dba0e30f993a6e43163b3fa53889242ed6c8f79a
Tharani318 <94125072+Tharani318@users.noreply.github.com>
2021-11-11 20:43:45 +0530

    Add files via upload

---
+++ CreateContact.java
@@ -0,0 +1,33 @@
+package assignments.day3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateContact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Contacts")).click();
+		driver.findElement(By.linkText("Create Contact")).click();
+		driver.findElement(By.id("firstNameField")).sendKeys("Tharani");
+		driver.findElement(By.id("lastNameField")).sendKeys("M");
+		driver.findElement(By.name("submitButton")).click();
+		String fn = driver.findElement(By.id("viewContact_firstName_sp")).getText();
+		System.out.println(fn);
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.close();
+
+	}
+
+}
